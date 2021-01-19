0fce56fba074b07a44aad516d376bb681e1276fe
Diego S. Alves <diegosantos57@outlook.com.br>
2021-01-18 22:45:57 -0300

    abs, round

---
+++ classes/len, abs, sum, round.py
@@ -0,0 +1,23 @@
+"""
+dunder len
+
+"""
+
+print('geek university'.__len__())
+
+#orientação a objetos, funçoes especiais
+
+"""
+abs = retorna o valor absoluto de um numero inteirou ou real
+retorna tudo positivo
+nao funcioona com strings, apenas numeros
+
+sum = soma
+retorna um número arredondado para n digito de precisao apos a casa decimal.
+
+
+
+"""
+print(round(10.72131232, 2))
+
+print(round(10.72131232))
