5f3ef96ef99b26dc6e85d62bedd7014d87309a0a
Diego S. Alves <diegosantos57@outlook.com.br>
2021-01-19 09:29:46 -0300

    errors

---
+++ classes/errors.py
@@ -0,0 +1,24 @@
+"""
+Erros mais comuns, saídas de erros:
+
+SyntaxError > erro de sintaxe
+
+NameError > variavel ou funçao nao definida
+
+TypeError > Quando uma funçao/operação é aplicada a um tipo errado
+
+IndexError >  índice invalido ao tentar acessar elemento
+
+ValyeError > Quando uma função/operação built-in recebe argumento com tipo correto mas valor inapropriado.
+
+ex:
+print(int('Geek')
+
+Keyerror > Ocorre quanto tentamos acessar um dicionária com uma chave que não existe.
+
+AttributeError > Ocorre quando uma variável não tem um atributo/função.
+
+Exception=erro
+"""
+
+
