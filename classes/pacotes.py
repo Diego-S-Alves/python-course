1c325b4a1e567e5c72d695cfeeff7eb5a81086f9
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-09 15:59:43 -0300

    pacotes

---
+++ classes/pacotes.py
@@ -0,0 +1,10 @@
+"""
+módulo é apenas um arquivo python.
+
+já um pacote é um diretório contendo uma coleção de módulos
+obs: nas versões menores que 3.0 um pacote deveria ter um arquivo chamado __init__.py
+a partir do python 3.x não é mais obrigatória, mas ainda é utilizado para manter compatibilidade.
+
+importação ocorre da mesma forma que nos módulos, pacote deve estar num diretório local
+
+"""
