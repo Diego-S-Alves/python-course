aee847dd6efd10efcf1ed3c7cdd20b6da2ae90b6
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-11 19:34:11 -0300

    dir

--- classes/dunder main e dunder name.py
+++ classes/dunder main e dunder name.py
@@ -3,5 +3,9 @@ dunder = __ double under
 dunder name --> __name__
 dunder main --> __main__
 
+__ para nao gerar conflito com os nomes dos elementos na programação
+
 
 """
+
+#if __name__ = '__main__':
