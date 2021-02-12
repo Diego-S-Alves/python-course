e092ee391b3ccf476e0c4b64545095a252084afe
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-12 18:25:56 -0300

    2

--- classes/leitura de arquivos.py
+++ classes/leitura de arquivos.py
@@ -1,4 +1,7 @@
 """
 para ler o conteudo usamos a funçao integrada open()
 
+apenas um parametro de entrada(caminho do arquivo) na forma mais simples,
+retorna um _io.TextIOWraper e é com ele que trabalhamos. Há varios parametros opcionais.
+
 """
