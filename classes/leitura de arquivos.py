e40e26e10e7a393d6a662c1ab92487572b0edefa
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-12 18:32:22 -0300

    read

--- classes/leitura de arquivos.py
+++ classes/leitura de arquivos.py
@@ -3,10 +3,14 @@ para ler o conteudo usamos a funçao integrada open()
 
 apenas um parametro de entrada(caminho do arquivo) na forma mais simples,
 retorna um _io.TextIOWraper e é com ele que trabalhamos. Há varios parametros opcionais.
+open abre somente para leitura
 
+read() - > ler o conteudo do arquivo
 """
 arquivo = open('forms_script_template')
 
 print(arquivo)
 
 print(type(arquivo))
+
+print(arquivo.read())
