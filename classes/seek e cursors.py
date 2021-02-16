8f33a41996c2cbfb1b9148a2cfe32ef4192f01b9
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-16 09:55:56 -0300

    read

--- classes/seek e cursors.py
+++ classes/seek e cursors.py
@@ -1,7 +1,17 @@
 """
 seek() movimenta o cursor pelo arquivo
+tem que usar o open novamente para atualizar o arquivo
 
 """
 
 arquivo =open("zen_of_python")
 print(arquivo.read())
+
+arquivo.seek(0)
+#seek() recebe um parametro que indica onde queremos colocar o cursor
+#readline() lê linha a linha
+arquivo.seek(0)
+x = arquivo.readline()
+print(x.split(' '))
+
+#readlines -> quantidade de linhas
