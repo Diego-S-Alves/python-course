f6fc9fc479f35f7d6c454a1f3a5b72d3f70c78de
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-16 10:04:50 -0300

    \n

--- classes/writing_in_files.py
+++ classes/writing_in_files.py
@@ -2,3 +2,7 @@
 ao abrir modo leitura se pode apenas ler e vice versa.
 
 """
+
+with open('zen_of_python', 'w') as arquivo: #modo de abertura do arquivo 'w'
+    arquivo.write('teste de dados novos no arquivo. \n')
+    arquivo.write('quantas linhas quisermos. \n')
