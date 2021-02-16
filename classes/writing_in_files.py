b9702353f029e76da39e258a166301711d513d13
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-16 10:11:03 -0300

    a

--- classes/writing_in_files.py
+++ classes/writing_in_files.py
@@ -1,10 +1,13 @@
 """
 ao abrir modo leitura se pode apenas ler e vice versa.
 ao abrir um arquivo para escrita o arquivo é criado
+argumento sempre string
+caso já exista o arquivo ele é apagado e um novo criado!!!
 """
 
 with open('teste', 'w') as arquivo: #modo de abertura do arquivo 'w'
     arquivo.write('teste de dados novos no arquivo. \n')
     arquivo.write('quantas linhas quisermos. \n')
 
+#tratamento de strings funciona normalmente
 
