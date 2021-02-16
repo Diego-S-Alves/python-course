a830c7765caa2130c6f5f94c56f2ebbae3ebf0c9
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-16 10:01:32 -0300

    bloco_with

--- classes/bloco_with.py
+++ classes/bloco_with.py
@@ -1,3 +1,10 @@
 """
+contexto de trabalho onde os recursos utilizados são fechado após o bloco
 
 """
+
+with open('zen_of_python') as arquivo:
+    print(arquivo.readlines())
+
+#forma pythonica de manipular arquivos
+#não precisa mais fechar o arquivo
