40ae6bc8604a2cb90edab8a14464d745dfe1f263
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-17 18:07:13 -0300

    iter

--- classes/iteradores e iteraveis.py
+++ classes/iteradores e iteraveis.py
@@ -1,3 +1,16 @@
 """
+iterator -> um objeto que pode ser iterado.
+objeto que retorna um dado sendo um elemento por   quando uma funçao next() é chamada.
 
+
+
+iterable ->
+-um objeto que irá retornar um iterator quando a funçao iter() for chamada
+listas- variaveis, dicionarios, tuplas
 """
+
+nome = 'Geek'
+
+it1 = iter(nome)
+
+print(next(it1))
