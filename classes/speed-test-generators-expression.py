f6e991f667f9bd7de7514f00fbe172f2ae42ee2a
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-19 12:37:09 -0300

    k,

--- classes/speed-test-generators-expression.py
+++ classes/speed-test-generators-expression.py
@@ -3,6 +3,11 @@ teste de tempo de processamento
 
 ex:
 """
+import time
 
-ge2 = (num for num in range(1,10))
+gen_inicio = time.time()
+
+ge2 = (num for num in range(100000000))
 print(ge2)
+#generator tem maior ganho em tempo de execuçao comparado com o list comprehension, fora a diferença em memoria
+
