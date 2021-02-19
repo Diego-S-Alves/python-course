b0a4a7f431bf699269422e9f906eb0f52cf13857
Diego S. Alves <diegosantos57@outlook.com.br>
2021-02-19 11:51:03 -0300

    j

--- classes/geradores.py
+++ classes/geradores.py
@@ -1,3 +1,9 @@
 """
+generators are iterators
+obs, o contrario não é verdadeiro, nem todo iterator é um generator
 
+funçoes geradores podem criar generators
+palavra reservada "yield"
+expressoes geradoras
+diferenças entre:
 """
