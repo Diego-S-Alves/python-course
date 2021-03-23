a95d0a89cb6dce90ec57c25105ce6de5943ccf25
Diego S. Alves <diegosantos57@outlook.com.br>
2021-03-22 23:04:35 -0300

    sim

--- simple exercises/File 3.py
+++ simple exercises/File 3.py
@@ -1,1 +1,8 @@
- 
+def soma(a,b):
+    return a + b
+
+
+
+f = int(input('Digite um número!'))
+s = int(input('Digite outro número!'))
+print(f'A soma dos números é igual a {soma(f,s)}')
--- simple exercises/File 4.py
+++ simple exercises/File 4.py
@@ -1,1 +1,6 @@
- 
+larg = float(input('Insira a largura da parede: '))
+alt = float(input('Insira a altura da parede: '))
+tint = (larg * alt) / 2
+
+print(f'Sua parede tem a dimensão de {larg}x{alt} e sua área é de {(larg*alt):.2f} metros quadrados.')
+print(f'Necessários {tint} litros de tinta.')
