7ad23575b2532f5232d7c74ab2e227333a6eb30b
Diego S. Alves <diegosantos57@outlook.com.br>
2021-03-22 22:05:16 -0300

    l

--- classes/Funções de maior grandeza.py
+++ classes/Funções de maior grandeza.py
@@ -1,3 +1,14 @@
 """
+high order functions - hof
+
+funçoes que retornam outras funçoes como resultado ou mesmo que podemos passar
+funçoes como argumnetos para outras funçoes e até mesmo criar variaveis do tipo de funçoes nos
+nossos programas.
+
+
 
 """
+
+# definindo funçoes
+def somar(a,b):
+    return a+b
