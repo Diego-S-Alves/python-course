5e3a57640807ac71421c41f6921035789b1f2b25
Diego S. Alves <diegosantos57@outlook.com.br>
2021-03-22 22:07:19 -0300

    nested functions

--- classes/Funções de maior grandeza.py
+++ classes/Funções de maior grandeza.py
@@ -5,10 +5,16 @@ funçoes que retornam outras funçoes como resultado ou mesmo que podemos passar
 funçoes como argumnetos para outras funçoes e até mesmo criar variaveis do tipo de funçoes nos
 nossos programas.
 
-
+em python as funçoes sao first class citizens
 
 """
 
 # definindo funçoes
 def somar(a,b):
     return a+b
+
+def calcular(num1, num2, funcao):
+    return funcao(num1 + num2)
+
+# nested functions
+
