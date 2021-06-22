9779015b76312dcf5b3c1d8ce6734e35ddf619bc
Diego S. Alves <diegosantos57@outlook.com.br>
2021-06-22 14:31:16 -0300

    Update scratch.py

--- aulas/scratch.py
+++ aulas/scratch.py
@@ -1,26 +1,16 @@
-matriz_1 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-matriz_2 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+A = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
 
-matriz_3 = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-
-for linha in range(4):
-    for coluna in range(4):
-        matriz_1[linha][coluna] = int(input("informe os numeros da matriz  1 :"))
-
-for linha in range(4):
-    for coluna in range(4):
-        matriz_2[linha][coluna] = int(input("informe os numeros da matriz 2 : "))
-
-for impressao_matriz in matriz_1, matriz_2:
-    print(impressao_matriz)
-
-print('\n')
-
-for linha in range(4):
-    for coluna in range(4):
-        if matriz_1[linha][coluna] > matriz_2[linha][coluna]:
-            matriz_3[linha][coluna] = matriz_1[linha][coluna]
+for i in range(0, 9):
+    for j in range(0, 9):
+        if i == j:
+            A[i][j] = 3 * i * i - 1
+        elif i < j:
+            A[i][j] = 2 * i + 7 * j - 2
         else:
-            matriz_3[linha][coluna] = matriz_2[linha][coluna]
+            A[i][j] = 4 * i * i - 5 * j * j + 1
 
-print(matriz_3)
+for impressao in A:
+    print(impressao)
