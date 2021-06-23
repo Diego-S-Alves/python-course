e880d4b6da46138a78521acad56ab5f6da1742e5
Diego S. Alves <diegosantos57@outlook.com.br>
2021-06-23 11:14:49 -0300

    Update scratch.py

--- aulas/scratch.py
+++ aulas/scratch.py
@@ -1,21 +1,26 @@
-class fibonacci():\n",
-    "    def __init__(self):\n",
-    "        self.quant_termos= int(input('Quantos termos da sequencia?:'))\n",
-    "        self.n1=0\n",
-    "        self.n2=1\n",
-    "    def gerar_sequencia(self):\n",
-    "        if self.quant_termos <=0:\n",
-    "            return 0\n",
-    "        elif self.quant_termos == 1:\n",
-    "            print('Primeiro termo da sequencia é ', self.quant_termos, ':' )\n",
-    "        else:\n",
-    "            count=0\n",
-    "            f_sqcia=[]\n",
-    "            print(f\"Sequência de Fibonacci com {self.quant_termos}:\")\n",
-    "            while count < self.quant_termos:\n",
-    "                f_sqcia.append(self.n1)\n",
-    "                nth = self.n1 + self.n2\n",
-    "                self.n1 = self.n2\n",
-    "                self.n2 = nth\n",
-    "                count += 1\n",
-    "            print(f_sqcia)\n
+class fibonacci():
+    def __init__(self):
+        self.quant_termos = int(input('Quantos termos da sequencia?:'))
+        self.n1 = 0
+        self.n2 = 1
+
+    def gerar_sequencia(self):
+        if self.quant_termos <= 0:
+            return 0
+        elif self.quant_termos == 1:
+            print('Primeiro termo da sequencia é ', self.quant_termos, ':')
+        else:
+            count = 0
+            f_sqcia = []
+            print(f"Sequência de Fibonacci com {self.quant_termos}:")
+            while count < self.quant_termos:
+                f_sqcia.append(self.n1)
+                nth = self.n1 + self.n2
+                self.n1 = self.n2
+                self.n2 = nth
+                count += 1
+            print(f_sqcia)
+
+
+sequencia = fibonacci()
+print(sequencia)
