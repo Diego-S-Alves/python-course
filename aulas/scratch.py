5c36444a183c6e11a39582a9d2ea98739fcccad0
Diego S. Alves <diegosantos57@outlook.com.br>
2021-06-24 16:41:17 -0300

    Update scratch.py

--- aulas/scratch.py
+++ aulas/scratch.py
@@ -1,26 +1,6 @@
-class fibonacci():
-    def __init__(self):
-        self.quant_termos = int(input('Quantos termos da sequencia?:'))
-        self.n1 = 0
-        self.n2 = 1
-
-    def gerar_sequencia(self):
-        if self.quant_termos <= 0:
-            return 0
-        elif self.quant_termos == 1:
-            print('Primeiro termo da sequencia é ', self.quant_termos, ':')
-        else:
-            count = 0
-            f_sqcia = []
-            print(f"Sequência de Fibonacci com {self.quant_termos}:")
-            while count < self.quant_termos:
-                f_sqcia.append(self.n1)
-                nth = self.n1 + self.n2
-                self.n1 = self.n2
-                self.n2 = nth
-                count += 1
-            print(f_sqcia)
-
-
-sequencia = fibonacci()
-print(sequencia)
+carrinho = []
+produto1 = {'nome': 'Playstation 4', 'quantidade': 1, 'preço': 2.300}
+produto2 = {'nome': 'GOW', 'quantidade': 1, 'preço': 150}
+carrinho.append(produto1)
+carrinho.append(produto2)
+print(carrinho)
