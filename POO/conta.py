faed8bf4e2099d7a55fee4b733346a98f4764869
Diego S. Alves <diegosantos57@outlook.com.br>
2021-06-25 10:19:59 -0300

    Update conta.py

--- POO/conta.py
+++ POO/conta.py
@@ -1,4 +1,9 @@
 class Conta:
 #função construtora
     def __init__(self):
-        print("Construindo objeto...")
+        print("Construindo objeto... {}".format(self))
+        self .numero = 123
+        self.titular = "Diego"
+        self.saldo = 55.0
+        self.limite = 1000.0
+        
