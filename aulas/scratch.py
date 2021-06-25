2676ecc4a23e4d32456a6efdda028559317c26f9
Diego S. Alves <diegosantos57@outlook.com.br>
2021-06-25 10:04:06 -0300

    poo

--- aulas/scratch.py
+++
@@ -1,6 +0,0 @@
-carrinho = []
-produto1 = {'nome': 'Playstation 4', 'quantidade': 1, 'preço': 2.300}
-produto2 = {'nome': 'GOW', 'quantidade': 1, 'preço': 150}
-carrinho.append(produto1)
-carrinho.append(produto2)
-print(carrinho)
